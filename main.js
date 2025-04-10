70175e10bcf22ee6fa3daff3673facc78bfc59f0
tedkatapetrov1 <teodor.petrov@abv.bg>
2025-04-10 11:09:16 +0300

    js change functions

--- main.js
+++ main.js
@@ -4,18 +4,16 @@ let mybutton = document.getElementById("myBtn");
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
-// Sho/hide button
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      button.style.display = "block"; // Показва бутона
-    } else {
-      button.style.display = "none"; // Скрива бутона
-    }
-  };
-  
-  // СScroll to the top
-  function topFuncion() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
   }
-  
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
