a7bf0cc15f82c8e95d110dd6c16d5aad21f5a5f6
tedkatapetrov1 <teodor.petrov@abv.bg>
2025-04-10 10:43:37 +0300

    from 100px to 20px change for scroll buton

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Показва/скрива бутона при скрол
 window.onscroll = function() {
     var button = document.getElementById("scrollToTopBtn");
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
       button.style.display = "block"; // Показва бутона
     } else {
       button.style.display = "none"; // Скрива бутона
